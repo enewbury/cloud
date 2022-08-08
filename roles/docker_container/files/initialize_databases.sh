934d34391d1c01951dcbc17e7828c4ad5b2e42d8
Eric Newbury <eric@newbury.cloud>
2022-08-07 22:54:39 -0400

    add in dump loading when present

--- roles/docker_container/files/initialize_databases.sh
+++ roles/docker_container/files/initialize_databases.sh
@@ -13,11 +13,23 @@ function create_user_and_database() {
 EOSQL
 }
 
+function load_dump_file() {
+	IFS=: read database password locale<<< "$1"
+	echo " Checking for dump file postgres_dump_$database.sql"
+	if [[ -f "/dump/postgres_dump_$database.sql" ]]; then
+		echo "  Loading $database dump file"
+		psql -U "$database" -d "$database" < "/dump/postgres_dump_$database.sql"
+	else
+		echo " No dump file found"
+	fi
+
+}
+
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
 		create_user_and_database $db
+		load_dump_file $db
 	done
 	echo "Multiple databases created"
 fi
-
